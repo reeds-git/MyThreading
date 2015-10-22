c8a9efb166482fd25b0df941c13c1e88ea416f7b
just4funbob <freefreefree2013@gmail.com>
2015-10-22 15:22:26 -0600

    deleted loader class, create class, and clear class and added onclick listeners for create, load, and clear buttons

--- app/src/main/java/cool/reed/cs246/mytreading/Clear.java
+++
@@ -1,7 +0,0 @@
-package cool.reed.cs246.mytreading;
-
-/**
- * Created by 1 on 10/22/2015.
- */
-public class Clear {
-}
--- app/src/main/java/cool/reed/cs246/mytreading/MainPage.java
+++ app/src/main/java/cool/reed/cs246/mytreading/MainPage.java
@@ -2,7 +2,9 @@ package cool.reed.cs246.mytreading;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import java.io.File;
 
 
 public class MainPage extends AppCompatActivity {
@@ -14,6 +16,24 @@ public class MainPage extends AppCompatActivity {
         setContentView(R.layout.activity_main_page);
     }
 
-    
+    public void writeFile(String filename) {
+        File file = new File(getFilesDir(), filename);
+
+    }
+
+    public void clickCreate(View view) {
+        System.out.println("I was created!!!!!!!");
+
+    }
+
+    public void clickLoad(View view) {
+        System.out.println("I was loaded!!!!!!!");
+
+    }
+
+    public void clickClear(View view) {
+        System.out.println("I was cleared!!!!!!!");
+
+    }
 
 }
