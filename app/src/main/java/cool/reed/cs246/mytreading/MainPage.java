1d0125f7d290680858e2fbd370467586c5a46de8
just4funbob <freefreefree2013@gmail.com>
2015-10-22 16:16:05 -0600

    added the clickCreate fuction to write to a file

--- app/src/main/java/cool/reed/cs246/mytreading/MainPage.java
+++ app/src/main/java/cool/reed/cs246/mytreading/MainPage.java
@@ -3,26 +3,40 @@ package cool.reed.cs246.mytreading;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class MainPage extends AppCompatActivity {
 
-    String filename = "numbers.txt";
+    public final String FILE_NAME = "numbers.txt";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
     }
 
-    public void writeFile(String filename) {
-        File file = new File(getFilesDir(), filename);
-
-    }
 
     public void clickCreate(View view) {
-        System.out.println("I was created!!!!!!!");
+
+
+        // create a FileWriter to write from the file
+        try (FileWriter fileWriter = new FileWriter(new File(getFilesDir(), FILE_NAME))) {
+            // add the numbers 1 - 10 to a file
+            for (int i = 1; i <= 10; i++) {
+                fileWriter.write(i + System.getProperty("line.separator"));
+
+                Thread.sleep(250);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            System.out.println("you will never get here unless you broke it bad");
+        }
 
     }
 
