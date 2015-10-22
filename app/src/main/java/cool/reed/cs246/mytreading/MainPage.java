4bf4afeff50d5bbe020b82fa2e7c1d500cf220b6
Reed <atw06001@byui.edu>
2015-10-22 09:22:45 -0600

    add filename needs fixing

--- app/src/main/java/cool/reed/cs246/mytreading/MainPage.java
+++ app/src/main/java/cool/reed/cs246/mytreading/MainPage.java
@@ -1,13 +1,19 @@
 package cool.reed.cs246.mytreading;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.File;
 
 public class MainPage extends AppCompatActivity {
 
+    String filename = numbers.txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
     }
+
+    
+    File file = new File(context.getFileDir(), filename);
 }
